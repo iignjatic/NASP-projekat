548cc9e058d718b616128f96b40346a4082cf10c
Ivana Ignjatic <ignjaticivana70@gmail.com>
2024-12-14 14:45:43 +0100

    [make SSTable function]

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module SSTable
+module NASP-PROJEKAT
 
 go 1.23.2
---
+++ main/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"NASP-PROJEKAT/data"
+	"NASP-PROJEKAT/sstable"
+	"fmt"
+)
+
+func main() {
+	table := sstable.SSTable{
+		DataSegment: &sstable.DataSegment{BlockSize: 5, SegmentSize: 10},
+	}
+
+	records := []data.Record{
+		{Crc: 12345, KeySize: 3, ValueSize: 5, Key: "key1", Value: []byte("val1"), Tombstone: false, Timestamp: "2024-06-14"},
+		{Crc: 67890, KeySize: 3, ValueSize: 5, Key: "key2", Value: []byte("val2"), Tombstone: true, Timestamp: "2024-06-15"},
+		{Crc: 54321, KeySize: 3, ValueSize: 5, Key: "key3", Value: []byte("val3"), Tombstone: false, Timestamp: "2024-06-16"},
+		{Crc: 12345, KeySize: 3, ValueSize: 5, Key: "key1", Value: []byte("val1"), Tombstone: false, Timestamp: "2024-06-14"},
+		{Crc: 67890, KeySize: 3, ValueSize: 5, Key: "key2", Value: []byte("val2"), Tombstone: true, Timestamp: "2024-06-15"},
+		{Crc: 54321, KeySize: 3, ValueSize: 5, Key: "key3", Value: []byte("val3"), Tombstone: false, Timestamp: "2024-06-16"}, {Crc: 12345, KeySize: 3, ValueSize: 5, Key: "key1", Value: []byte("val1"), Tombstone: false, Timestamp: "2024-06-14"},
+		{Crc: 67890, KeySize: 3, ValueSize: 5, Key: "key2", Value: []byte("val2"), Tombstone: true, Timestamp: "2024-06-15"},
+		{Crc: 54321, KeySize: 3, ValueSize: 5, Key: "key3", Value: []byte("val3"), Tombstone: false, Timestamp: "2024-06-16"},
+	}
+
+	var recordPtrs []*data.Record
+	for i := range records {
+		recordPtrs = append(recordPtrs, &records[i])
+	}
+	table.MakeSSTable(recordPtrs)
+	fmt.Println("SSTable sa segmentima: ", table.DataSegment.Blocks)
+
+}
