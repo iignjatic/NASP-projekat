a8aa7f7d3aaa34a72b895ea03b9434e2bdb0d548
Milan Kačarević <milankacarevic007@gmail.com>
2025-01-22 12:40:29 +0100

    save segment

--- wal/wal.go
+++ wal/wal.go
@@ -1,6 +1,9 @@
 package wal
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 const DIRECTORY_PATH = "segments"
 
@@ -29,6 +32,7 @@ func (w *Wal) AddNewSegment() {
 	w.CurrentSegment = newSegment
 	w.SegmentPaths = append(w.SegmentPaths, newSegment.FilePath)
 	fmt.Printf("Created new Segment %s\n", newSegment.FilePath)
+	w.PrintCurrentWalSegmentsID()
 }
 
 func (w *Wal) AddRecord(record *Record) {
@@ -42,6 +46,42 @@ func (w *Wal) AddRecord(record *Record) {
 func (w *Wal) FlushCurrentSegment() {
 	if w.CurrentSegment != nil {
 		fmt.Printf("Flushing segment %s to disk...\n", w.CurrentSegment.FilePath)
+		w.WriteSegmentToFile(w.CurrentSegment)
 		w.CurrentSegment.Transferred = true
 	}
+}
+
+func (w *Wal) WriteSegmentToFile(s *Segment) error {
+	filePath := w.DirectoryPath + "/" + s.FilePath
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	for i:=0;i<len(s.Blocks);i++ {
+		for j:=0;j<len(s.Blocks[i].Records);j++ {
+			recordBytes, err := s.Blocks[i].Records[j].ToBytes()
+			if err != nil {
+				return err
+			}
+			_, err = file.Write(recordBytes)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// func (w *Wal) RemoveSegment(ID int) []*Segment {
+// 	return append(w.Segments[:ID], w.Segments[ID+1:]...)
+// }
+
+func (w *Wal) PrintCurrentWalSegmentsID() {
+	fmt.Print("Current Wal Segment IDs: ")
+	for i:=0;i<len(w.Segments);i++ { 
+		fmt.Printf("%d ", w.Segments[i].ID)
+	}
+	fmt.Println()
 }
