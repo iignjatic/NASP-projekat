3996d3a0857b24ee8394aba15cf7d9793c8cae21
Ivana Ignjatic <ignjaticivana70@gmail.com>
2025-01-25 14:15:55 +0100

    [Block Cache]

--- BlockCache/BlockCache.go
+++ BlockCache/BlockCache.go
@@ -2,8 +2,9 @@ package BlockCache
 
 type BlockNode struct {
 	Block *Block
-	Prev *BlockNode
-	Next *BlockNode
+	Key   uint32 //redni broj bloka
+	Prev  *BlockNode
+	Next  *BlockNode
 }
 
 type LRUlist struct {
@@ -17,36 +18,54 @@ type BlockCache struct {
 	BlockMap map[uint32]*BlockNode
 }
 
-func (cache *BlockCache) checkCache(key uint32) block *Block{
-	_, exists := cache.blockMap[int(key)]
+func (cache *BlockCache) checkCache(key uint32) *Block {
+	_, exists := cache.BlockMap[key]
 	if exists {
-		return cache.blockMap.block
+		return cache.BlockMap[key].Block
 	} else {
 		return nil
 	}
-} 
+}
 
-func (cache *BlockCache) addCache(key uint32, block *Block){
-	node, exists := cache.blockMap[int(key)]
+func (cache *BlockCache) addCache(key uint32, block *Block) {
+	node, exists := cache.BlockMap[key]
 	if exists {
-		node.Prev.Next = node.Next
-		node.Next.Prev = node.Prev
-		node.Next = cache.LRUlist.Head.Next
-		cache.LRUlist.Head = newNode
-		
+		if cache.LRUlist.Head != node {
+			node.Prev.Next = node.Next
+			node.Next.Prev = node.Prev
+			node.Next = cache.LRUlist.Head.Next
+			cache.LRUlist.Head.Prev = node
+			cache.LRUlist.Head = node
+		}
+
 	} else {
 		newNode := &BlockNode{
 			Block: block,
+			Key:   key,
 			Prev:  nil,
-			Next:  cache.LRUlist.head,
+			Next:  cache.LRUlist.Head,
 		}
-		newNode.Next = cache.LRUlist.Head.Next
-		cache.LRUlist.Head = newNode
+		if cache.LRUlist.Head == nil {
+			cache.LRUlist.Head = newNode
+			cache.LRUlist.Tail = newNode
 
-		LRUlist.Tail.Prev.Next = nil
-		delete(cache.BlockMap, cache.LRUlist.Tail.Block.Key)
+		} else if cache.LRUlist.Head == cache.LRUlist.Tail {
+			newNode.Next = cache.LRUlist.Head
+			cache.LRUlist.Head = newNode
+			cache.LRUlist.Tail.Prev = newNode
 
-
-		
+		} else {
+			newNode.Next = cache.LRUlist.Head
+			cache.LRUlist.Head.Prev = newNode
+			cache.LRUlist.Head = newNode
+			if cache.Capacity == 0 {
+				delete(cache.BlockMap, cache.LRUlist.Tail.Key)
+				cache.LRUlist.Tail = cache.LRUlist.Tail.Prev
+				cache.LRUlist.Tail.Next = nil
+			}
+		}
+	}
+	if cache.Capacity != 0 {
+		cache.Capacity--
 	}
 }
