c20effab2b29b7e951304986d38b1c529fe81b5e
Elena Vukovic <elenavukovic2004@gmail.com>
2025-01-29 16:51:09 +0100

    add delete and get functions

---
+++ memtable/memtableBTree.go
@@ -0,0 +1,128 @@
+package memtable
+
+import (
+	"NASP-PROJEKAT/b_tree"
+	"NASP-PROJEKAT/data"
+	"errors"
+	"fmt"
+	"time"
+)
+
+const ORDER = 4
+
+type MemtableB struct {
+	data        b_tree.BTree
+	maxSize     uint
+	readOnly    bool
+	currentSize uint
+}
+
+type MemtableManagerB struct {
+	tables      []*Memtable
+	maxTables   uint
+	oldestIndex uint
+	acitveIndex uint
+}
+
+func CreateMemtableB(maxSize uint, readOnly bool) *MemtableB {
+	return &MemtableB{
+		data:        *b_tree.NewBTree(ORDER),
+		maxSize:     maxSize,
+		readOnly:    readOnly,
+		currentSize: 0,
+	}
+}
+
+// dodavanje Record strukture u Memtable
+func (memt *MemtableB) AddRecord(record data.Record) error {
+	if memt.readOnly {
+		return errors.New("cannot add to a read-only memtable")
+	}
+
+	fmt.Printf("Dodavanje recorda sa kljucem %s\n", record.Key)
+
+	memt.data.InsertRecord(&record)
+	memt.currentSize++
+	return nil
+}
+
+// dodavanje novog para kljuc-vrijednost u memtable
+/*func (memt *MemtableB) AddNewRecord(key string, value []byte) error {
+	if memt.readOnly {
+		return errors.New("cannot add to a read-only memtable")
+	}
+
+	if memt.currentSize >= memt.maxSize {
+		_, err := memt.Flush()
+		if err != nil {
+			return err
+		}
+	}
+
+	record := data.Record{Key: key, Value: value, Tombstone: false, Timestamp: time.Now().UTC().Format(time.RFC3339), KeySize: uint64(len(key)), ValueSize: uint64(len(value))}
+	memt.data.InsertRecord(&record)
+	memt.currentSize++
+	return nil
+}*/
+
+// dobavljenje recorda prema kljucu iz jedne memtabele
+func (memt *MemtableB) Get(key string) (*data.Record, error) {
+	record, err := memt.data.Get(key)
+	if err != nil || record.Tombstone {
+		return nil, errors.New("key not found")
+	}
+
+	//fmt.Printf("Pronadjen record sa kljucem %s\n", key)
+	return record, nil
+}
+
+func (memt *MemtableB) IsFull() bool {
+	return memt.currentSize == memt.maxSize
+}
+
+// logicko brisanje recorda
+func (memt *MemtableB) Delete(key string) error {
+	if memt.readOnly {
+		return errors.New("cannot delete from a read-only memtable")
+	}
+
+	record, err := memt.data.Get(key)
+	if err != nil {
+		//fmt.Printf("Record za kljuc %s nije pronadjen, funkcija Delete()", key)
+		return err
+	}
+
+	record.Tombstone = true
+	record.Timestamp = time.Now().UTC().Format(time.RFC3339)
+	memt.data.InsertRecord(record)
+	return nil
+}
+
+// flush sortira podatke po kljucu
+// nakon upisivanja podataka na disk, oslobadja memtable
+/*func (memt *MemtableB) Flush() ([]data.Record, error) {
+	fmt.Println("Radi se Flush()")
+	if memt.currentSize == 0 {
+		return nil, errors.New("nothing to flush")
+	}
+
+	records := b_tree.NewBTree(ORDER)
+	for _, record := range memt.data {
+		records = append(records, *record)
+	}
+
+	// sortiranje
+	sort.Slice(records, func(i, j int) bool {
+		return records[i].Key < records[j].Key
+	})
+
+	// flushing data
+	// SSTable logic
+
+	fmt.Println("Flush() zapisani podaci na disku")
+
+	// praznjenje memtable
+	memt.data = make(map[string]*data.Record)
+	memt.currentSize = 0
+	return records, nil
+}*/
