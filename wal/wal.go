c10c9a6689e8854b02c534b7837cdd41e5aa624c
Milan Kačarević <milankacarevic007@gmail.com>
2025-02-10 17:06:15 +0100

    handle incomplete segments

--- wal/wal.go
+++ wal/wal.go
@@ -2,6 +2,7 @@ package wal
 
 import (
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -30,12 +31,57 @@ func NewWal() *Wal {
 
 func (w *Wal) AddNewSegment() {
 	w.SegmentPaths = w.ReadSegmentNames()
-	newSegmentID := len(w.SegmentPaths)
-	newSegment := NewSegment(newSegmentID)
-	w.Segments = append(w.Segments, newSegment)
-	w.CurrentSegment = newSegment
-	w.SegmentPaths = append(w.SegmentPaths, newSegment.FilePath)
-	fmt.Printf("Created new Segment %s\n", newSegment.FilePath)
+
+	// get the last segment
+	segmentNames := w.ReadSegmentNames()
+	var lastSegment string
+	if len(segmentNames) == 0 {
+		newSegmentID := len(w.SegmentPaths)
+		newSegment := NewSegment(newSegmentID)
+		w.Segments = append(w.Segments, newSegment)
+		w.CurrentSegment = newSegment
+		w.SegmentPaths = append(w.SegmentPaths, newSegment.FilePath)
+		fmt.Printf("Created new Segment %s\n", newSegment.FilePath)
+		return
+	}
+	lastSegment = segmentNames[len(segmentNames)-1]
+
+	// check if the last segment is not full by reading the first byte
+	ifFull, err := w.ReadFirstByte(lastSegment)
+	if err != nil && ifFull != 0 && ifFull != 1 {
+		err := errors.New("cannot read the first byte")
+		fmt.Println("Error: ", err) 
+		return
+	}
+
+	if ifFull == 0 {
+		// if the last segment is not full, make it current segment
+		segmentPath := filepath.Join(w.DirectoryPath, lastSegment)
+		records, err := w.ReadSegmentFromFile(segmentPath)
+		if err != nil {
+			fmt.Printf("Error reading records from segment %s: %v\n", segmentPath, err)
+			return
+		}
+		noZerosRecords := NoZerosRecords(records)
+		defragmentedRecords := DefragmentRecords(noZerosRecords)
+
+		// create the segment and add records to it
+		lastUsedSegment := NewSegment(len(w.SegmentPaths)-1)
+		w.Segments = append(w.Segments, lastUsedSegment)
+		w.CurrentSegment = lastUsedSegment
+		for _, record := range defragmentedRecords {
+			rec := NewRecord(record.Key, append([]byte{}, record.Value...))
+			w.AddRecord(rec)
+			w.CurrentSegment.PrintBlocks()
+		}
+	} else {
+		newSegmentID := len(w.SegmentPaths)
+		newSegment := NewSegment(newSegmentID)
+		w.Segments = append(w.Segments, newSegment)
+		w.CurrentSegment = newSegment
+		w.SegmentPaths = append(w.SegmentPaths, newSegment.FilePath)
+		fmt.Printf("Created new Segment %s\n", newSegment.FilePath)
+	}
 }
 
 func (w *Wal) ReadSegmentNames() []string {
@@ -57,9 +103,11 @@ func (w *Wal) AddRecord(record *Record) {
 	w.CurrentSegment.AddRecordToBlock(record)
 	if w.CurrentSegment.IsFull() {
 		w.FlushCurrentSegment()
+		w.HandleFullness(w.CurrentSegment)
 		w.AddNewSegment()
+	} else {
+		w.FlushCurrentSegment()
 	}
-	w.FlushCurrentSegment()
 }
 
 func (w *Wal) FlushCurrentSegment() {
@@ -78,8 +126,8 @@ func (w *Wal) WriteSegmentToFile(s *Segment) error {
 	}
 	defer file.Close()
 
-	// reserve eight bytes for number of bytes that are pushed on memtable from this segment
-	offset := make([]byte, 8)
+	// reserve one byte for indicator of fullness of the segment and eight bytes for number of bytes that are pushed on memtable from this segment
+	offset := make([]byte, 9)
 	binary.LittleEndian.PutUint64(offset, 0)
 	_, err = file.Write(offset)
 	if err != nil {
@@ -101,6 +149,26 @@ func (w *Wal) WriteSegmentToFile(s *Segment) error {
 	return nil
 }
 
+// just change the fist byte to one if the segment is full
+func (w *Wal) HandleFullness(s *Segment) error {
+	file, err := os.OpenFile(w.DirectoryPath + "/" + s.FilePath, os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.Seek(0, io.SeekStart)
+	if err != nil {
+		return fmt.Errorf("failed to seek to beginning of the file: %w", err)
+	}
+	offset := []byte{1}
+	_, err = file.Write(offset)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 // a function that reads the whole segment record by record, or from a particular position in the file
 func (w *Wal) ReadSegmentFromFile(filePath string) ([]*Record, error) {
 	file, err := os.Open(filePath)
@@ -108,6 +176,12 @@ func (w *Wal) ReadSegmentFromFile(filePath string) ([]*Record, error) {
 		return nil, fmt.Errorf("failed to open file %s: %w", filePath, err)
 	}
 	defer file.Close()
+
+	// move the pointer to the second byte - first byte is for fullness of the file
+	_, err = file.Seek(1, io.SeekStart)
+	if err != nil {
+		return nil, fmt.Errorf("failed to seek to first byte: %w", err)
+	}
 	
 	// read offset
 	offsetBytes := make([]byte, 8)
@@ -160,6 +234,21 @@ func (w *Wal) ReadSegmentFromFile(filePath string) ([]*Record, error) {
 	return records, nil
 }
 
+func (w *Wal) ReadFirstByte(segmentPath string) (byte, error) {
+	file, err := os.OpenFile(w.DirectoryPath + "/" + segmentPath, os.O_RDONLY, 0644)
+	if err != nil {
+		return 2, err
+	}
+	defer file.Close()
+
+	buffer := make([]byte, 1)
+	_, err = file.Read(buffer)
+	if err != nil {
+		return 2, fmt.Errorf("failed to read first byte: %w", err)
+	}
+	return buffer[0], nil
+}
+
 func (w *Wal) ReadAllSegments() ([]*Record, error) {
 	var allRecords []*Record
 	for _, segmentName := range w.ReadSegmentNames() {
@@ -201,7 +290,7 @@ func DefragmentRecords(r []*Record) []*Record {
 			record.Value = append(record.Value, r[i].Value...)
 		}
 		if r[i].Type == 'm' {
-			record.Value = append(record.Value, r[i].Value...)
+			record.Value = append(record.Value, r[i].Value...  )
 		} else if r[i].Type == 'l' {
 			record.Value = append(record.Value, r[i].Value...)
 			temp = append(temp, record)
@@ -211,5 +300,6 @@ func DefragmentRecords(r []*Record) []*Record {
 	return temp
 }
 
-// kada upisujem podatke u novom pokretanju programa, treba da se dodaju novi segment fajlovi, sto i jeste istina PLUS da se kupi posljednji segment koji nije pun, i da se na njega nastavi pisanje,..
+// kada upisujem podatke u novom pokretanju programa, treba da se dodaju novi segment fajlovi, sto i jeste istina PLUS da se kupi posljednji segment koji nije pun, i da se na njega nastavi pisanje,.. posle svakog sacuvavanja,
 // pad sistema
+// povezi i dodaj funkcionalnost prebacivanja memtabeli i funkcionalnost za mijenjanje onih 8 bajtova kod potvrde sstabele
